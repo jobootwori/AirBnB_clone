9abc099c42ddb49d17260dd86ece5a401adf3bc8
jobootwori <jobootwori@gmail.com>
2022-08-03 13:10:51 +0300

    UPDATE: Initial commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""BaseModel class"""
+
+import models
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """The BaseModel class will be inherited by other classes"""
+    def __init__(self, *rgs, **kwargs):
+        """__init__ method and instatianting the BaseModel class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        for name, value in kwargs.items():
+            """searches through dict for keys"""
+            if name == "__class__"
+                continue
+            setattr(self, name, value)
+        if "id" not in kwargs:
+            models.storage.new(self)
+
